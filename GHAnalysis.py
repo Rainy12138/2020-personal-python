b0e64cf8f34c5dce50faa20fa296546b68cae6fc
Fishbot12138 <1084711193@qq.com>
2020-09-13 23:32:25 +0800

    Update GHAnalysis.py

--- GHAnalysis.py
+++ GHAnalysis.py
@@ -1,1 +1,24 @@
-print( "Hello World!" )
+import json
+
+# 由于文件中有多行，直接读取会出现错误，因此一行一行读取
+file = open("papers.json", 'r', encoding='utf-8')
+papers = []
+creatnum=0
+pushnnum=0
+releasenum=0
+watchnum=0
+for line in file.readlines():
+    dic = json.loads(line)
+    papers.append(dic)
+    if dic['type']=='PushEvent':
+       pushnnum+=1
+    elif dic['type']=='ReleaseEvent':
+        releasenum+=1
+    elif dic['type']=='CreateEvent':
+        creatnum+=1
+    elif dic['type']=='WatchEvent':
+        watchnum+=1
+print(creatnum)
+print(pushnnum)
+print(releasenum)
+print(watchnum)
