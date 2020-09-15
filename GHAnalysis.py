58d601d2e24c715c037c5a37f78410e70a3bd438
Fishbot12138 <1084711193@qq.com>
2020-09-16 00:22:07 +0800

    Update GHAnalysis.py

--- GHAnalysis.py
+++ GHAnalysis.py
@@ -1,20 +1,36 @@
 import json
 import collections
-# 由于文件中有多行，直接读取会出现错误，因此一行一行读取
-file = open("papers.json", 'r', encoding='utf-8')
+import os
+
+
+
+input_=input()
+
+
+if input_=='-i'|'--init':
+     input_=input()
+     print(input_)
+    
+
+
+file = open("jason/papers.json", 'r', encoding='utf-8')
 
 papers = []
 data=[]
 
-idlist=[0 for i in range(20000)]
+idlist=[0 for i in range(200000)]
 idcount=0
-numlist=[0 for i in range(20000)]
+numlist=[0 for i in range(200000)]
 
 pushnnum=0
 commentnum=0
 issuesnum=0
 pullnum=0
+count=0
+count2=0
 
+search=input()
+search=int(search)
 
 
 for line in file.readlines():
@@ -22,38 +38,50 @@ for line in file.readlines():
     papers.append(dic)
 
 
-data=sorted(papers,key=lambda x:x['actor']['id']) 
+data=sorted(papers,key=lambda x:(x['actor']['id'],x['type'])) 
 
 for key in data:
-    if idlist[idcount]!=key['actor']['id']:
-        numlist[idcount]=pushnnum
-        numlist[idcount+1]=commentnum
-        numlist[idcount+2]=issuesnum
-        numlist[idcount+3]=pullnum
+
+  
+    if idlist[idcount]==key['actor']['id']:
+        if key['type']=='PushEvent':
+           pushnnum+=1
+        elif key['type']=='IssueCommentEvent':
+           commentnum+=1
+        elif key['type']=='IssuesEvent':
+           issuesnum+=1
+        elif key['type']=='PullRequestEvent':
+           pullnum+=1
+    else:  
+        numlist[idcount*4]=pushnnum
+        numlist[idcount*4+1]=commentnum
+        numlist[idcount*4+2]=issuesnum
+        numlist[idcount*4+3]=pullnum
         pushnnum=0
         commentnum=0
         issuesnum=0
         pullnum=0
+        idcount+=1
         idlist[idcount]=key['actor']['id']
-        idcount+=4
-    if key['type']=='PushEvent':
-        pushnnum+=1
-    elif key['type']=='IssueCommentEvent':
-        commentnum+=1
-    elif key['type']=='"IssuesEvent':
-        issuesnum+=1
-    elif key['type']=='PullRequestEvent':
-       pullnum+=1
-           
+        if key['type']=='PushEvent':
+           pushnnum+=1
+        elif key['type']=='IssueCommentEvent':
+           commentnum+=1
+        elif key['type']=='IssuesEvent':
+           issuesnum+=1
+        elif key['type']=='PullRequestEvent':
+           pullnum+=1
+        
+          
 while idcount!=0:
-    idcount-=4 
-    print(idlist[idcount])
-    print("PushEvent:",numlist[idcount-4])
-    print("IssueCommentEvent:",numlist[idcount-3])
-    print("IssuesEvent:",numlist[idcount-2])
-    print("PullRequestEvent:",numlist[idcount-1],"\n")
-      
+       if search==idlist[idcount]:
+           print(idlist[idcount])
+           print("PushEvent:",numlist[idcount*4])
+           print("IssueCommentEvent:",numlist[idcount*4+1])
+           print("IssuesEvent:",numlist[idcount*4+2])
+           print("PullRequestEvent:",numlist[idcount*4+3],"\n")
+       idcount-=1   
 
-
+  
 
 
